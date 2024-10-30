b067bd30934968ea8418563925808a84a80fdc15
Madson Santos <madsonsamsung@gmail.com>
2024-10-30 18:36:33 -0300

    creating initial layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "frontend-journey",
@@ -11,7 +12,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main className="flex min-h-screen bg-gray-100">
+          <aside className="bg-white w-64 p-6 border-r border-gray-200">
+            <div className="flex items-center space-x-2 mb-6 border-b border-gray-200 py-6">
+              <span>Dashboard</span>
+            </div>
+            <div className="flex-1 mt-6"></div>
+          </aside>
+
+          <section className="flex-1 p-8 bg-white mt-4 ml-4 mb-4 rounded-lg shadow-md border border-gray-200">
+            {children}
+          </section>
+        </main>
+      </body>
     </html>
   );
 }
